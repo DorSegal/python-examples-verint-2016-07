1a0c30e6e9b41e82a0f78b7e3214ec3792138312
Dor Segal <dor982@gmail.com>
2016-07-05 17:02:11 +0300

    Solution for: sol-python-verint.syntax-lab/01.py

--- 05_syntax_lab/01.py
+++ 05_syntax_lab/01.py
@@ -1,5 +1,14 @@
-"""
-Write a program that reads 10 numbers from
-the user and prints the largest one
-"""
+print "This program will get10 numbers for user and return the biggset one"
+
+i=0
+number=0
+temp=0
+for i in range(0,10):
+    print "Please enter number:",
+    temp=int(raw_input())
+    if number < temp: number=temp
+    print
+
+print number
+
 
