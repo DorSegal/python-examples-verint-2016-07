c0796b0b1710a3a4ce8198d9ac83308c70977c15
Dor Segal <dor982@gmail.com>
2016-07-06 10:26:50 +0300

    Update 01.py
    all changed.
    the extra print was just to print a new line after the user input

--- 05_syntax_lab/01.py
+++ 05_syntax_lab/01.py
@@ -1,14 +1,12 @@
 print "This program will get10 numbers for user and return the biggset one"
 
-i=0
 number=0
-temp=0
 for i in range(0,10):
-    print "Please enter number:",
-    temp=int(raw_input())
+    temp=int(raw_input("Please enter number:"))
     if number < temp: number=temp
-    print
-
 print number
+
+
+
 
 
