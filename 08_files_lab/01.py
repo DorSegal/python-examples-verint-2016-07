acc2aea715b74e6f37d9c03fae39e52ca3172f58
Dor Segal <dor982@gmail.com>
2016-07-12 18:21:41 +0300

    Solution for: sol-python-verint.files-lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,6 +1,13 @@
-"""
-Write a program that takes 2 file names
-and appends the second file's contents to
-the end of the first one
-"""
+import argparse
 
+parser = argparse.ArgumentParser(description="this progem copy contect from source file to destination file")
+parser.add_argument("srcfile", type=str ,help="the source file you want to copy")
+parser.add_argument("dstfile", type=str, help="destination file")
+args = parser.parse_args()
+
+
+with open(args.srcfile, "r") as srcfile:
+    with open(args.dstfile, "a") as dstfile:
+        for line in srcfile:
+            dstfile.write(line)
+        dstfile.write("\n")
