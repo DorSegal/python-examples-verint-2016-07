187d67cc22ca9307db79da4ae36908dda71a18ce
Dor Segal <dor982@gmail.com>
2016-07-12 18:21:42 +0300

    Solution for: sol-python-verint.files-lab/02.py

--- 08_files_lab/02.py
+++ 08_files_lab/02.py
@@ -1,7 +1,16 @@
-"""
-Write a program that takes 3 names. The first two
-are existing file names and the last is a new file name
-Program should write the lines from the first two 
-files interwinded into the output file
-"""
+import argparse
+import sys
+from itertools import izip_longest
 
+parser = argparse.ArgumentParser(description="this progem marge two files into destination file")
+parser.add_argument("srcfile1", type=str ,help="the first source file")
+parser.add_argument("srcfile2", type=str ,help="the second source file")
+parser.add_argument("dstfile", type=str, help="destination file containing marge content")
+
+args = parser.parse_args()
+
+with open(args.srcfile1, "r") as srcfile1:
+    with open(args.srcfile2, "r") as srcfile2:
+        with open(args.dstfile, "w") as dstfile:           
+            for line, line2 in izip_longest(srcfile1, srcfile2, fillvalue=""):
+                dstfile.write(line + line2)
