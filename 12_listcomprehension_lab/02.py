2dc37925b3ebc951b98f5656326819a3d5d4a255
Dor Segal <dor982@gmail.com>
2016-07-14 17:13:16 +0300

    Solution for: sol-python-verint.list-comprehension-lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -1,6 +1,13 @@
 """
-Write a python program that takes two words
-as sys.argv and prints only the letters
-common to both
+Find and print all possible sequences
+of 3 lowercase characters: aaa, aab, aac, ..., zzz
 """
+from collections import defaultdict
+import itertools
 
+anagrams = defaultdict(str)
+
+lowletters = [chr(l) for l in range(255) if chr(l).islower()]
+
+for letter in itertools.combinations_with_replacement(lowletters, 3): 
+        print(''.join(letter))
