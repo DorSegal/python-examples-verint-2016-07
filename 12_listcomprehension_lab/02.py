669742923191d75f8280980c9f72e4eae73eea3c
Dor Segal <dor982@gmail.com>
2016-07-14 17:31:04 +0300

    Update 02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -2,10 +2,7 @@
 Find and print all possible sequences
 of 3 lowercase characters: aaa, aab, aac, ..., zzz
 """
-from collections import defaultdict
 import itertools
-
-anagrams = defaultdict(str)
 
 lowletters = [chr(l) for l in range(255) if chr(l).islower()]
 
