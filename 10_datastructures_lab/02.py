1c865a8207cff4251bfd913d30dbf05834a551ea
Dor Segal <dor982@gmail.com>
2016-07-14 11:52:02 +0300

    Solution for: sol-python-verint.datastructures-lab/02.py

--- 10_datastructures_lab/02.py
+++ 10_datastructures_lab/02.py
@@ -1,5 +1,12 @@
-"""
-Write a program that prints only unique values
-from sys.argv (without program name)
-"""
+from collections import defaultdict
 
+anagrams = defaultdict(str)
+
+with open("file.txt" ,"r") as fin:
+    for line in fin:
+        word = line.rstrip("\n")
+        key = ''.join(sorted(word.lower()))
+        anagrams[key] += word + " "
+
+for item in anagrams:
+    print anagrams[item]
