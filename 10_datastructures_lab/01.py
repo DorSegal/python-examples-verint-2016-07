83a0ab6b2fbce0437a1b37c8243c634133b56a59
Dor Segal <dor982@gmail.com>
2016-07-14 11:54:17 +0300

    Update 01.py

--- 10_datastructures_lab/01.py
+++ 10_datastructures_lab/01.py
@@ -4,13 +4,11 @@ hostsfile = {}
 
 with open("hosts", "r") as fin:
     for line in fin:
-       (name, ip) = line.split('=')
+       (name, ip) = line.rstrip('\n').split('=')
        hostsfile[name] = ip
 
-
 for name in sys.argv[1:]:
-    try:
-        print "%s = %s" % (name, hostsfile[name])
-
-    except:
-        print "%s does not exist\n" % name
+    if name in hostsfile:
+        print "{0} IP is {1}".format(name, hostsfile[name])
+    else:
+        print "{0} does not exist".format(name)
