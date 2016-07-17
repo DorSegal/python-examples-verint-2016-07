38b6f2d66f7a2e2e9c3aa67d5b6a51b30cb5cc3f
Dor Segal <dor982@gmail.com>
2016-07-17 18:25:58 +0300

    Solution for: sol-python-verint.functions-lab/01.py

--- 14_functions_lab/01.py
+++ 14_functions_lab/01.py
@@ -5,3 +5,17 @@ Write 2 functions:
     Ignore non-numeric arguments
 """
 
+def mysum(*numbers):
+    return sum([num for num in numbers if type(num) == int])
+
+print mysum(10, 20, 30, "test")
+
+def mymul(*numbers):
+    if len(numbers) != 0:
+        mul = lambda x, y: x*y
+        return reduce(mul,[num for num in numbers if type(num) == int])
+    else: return 1
+
+print mymul()
+
+print mymul(10, "dor", 10 )
