bb959d230d816785305ec8af6e3b825a86c902c3
Dor Segal <dor982@gmail.com>
2016-07-18 22:13:46 +0300

    Solution for: sol-python-verint.functions-lab/03.py

--- 14_functions_lab/03.py
+++ 14_functions_lab/03.py
@@ -3,3 +3,11 @@ Write a function that calculates the sum
 of the 10th digit from all arguments passed to it
 """
 
+def asarotcount(*numbers):
+    count = 0
+    for num in numbers:
+        count += int(str(num)[-2])
+    return count
+
+
+print asarotcount(120, 230, 1150)
