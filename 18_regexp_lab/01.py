d6111536c4ade23372fb59eeec102c541c50c83e
Dor Segal <dor982@gmail.com>
2016-07-20 23:29:42 +0300

    Solution for: sol-python-verint.regexp-lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -9,4 +9,22 @@ Each line in the file can either be:
 Write a program that takes a property file name and key
 as command line arguments and prints the requested value
 """
+import re
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("file", type=str, help="The file you want to scan")
+parser.add_argument("key", type=str, help="sreach of key value")
+args = parser.parse_args()
+
+
+def getvaule(key, f):
+    #(?<=key = ).*
+    res = re.search(r"(?<=" + key + r' = ).*', f)
+    if res is not None:
+        return res.group(0)
+    else: return "key not found"
+
+with open(args.file, "r") as f:
+    print getvaule(args.key, f.read())
+    
